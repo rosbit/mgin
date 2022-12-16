90d45c302f4b7e89c3bc53b4a75a78006933187d
Rosbit Xu <me@rosbit.cn>
2022-12-16 09:44:35 +0800

    using rosbit/reader-logger

--- body-dumper.go
+++ body-dumper.go
@@ -1,17 +1,10 @@
 package mgin
 
 import (
+	logr "github.com/rosbit/reader-logger"
 	"io"
 )
 
 func bodyDumper(body io.Reader, dumper io.Writer) (reader io.Reader, deferFunc func()) {
-	if dumper == nil {
-		return body, func(){}
-	}
-
-	io.WriteString(dumper, "--- mgin dumping body begin ---\n")
-	r := io.TeeReader(body, dumper)
-	return r, func() {
-		io.WriteString(dumper, "\n--- mgin dumping body end ---\n")
-	}
+	return logr.ReaderLogger(body, dumper, "mgin dumping body")
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/rosbit/mgin
 go 1.12
 
 require (
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/validator/v10 v10.5.0 // indirect
+	github.com/gin-contrib/sse v0.1.0
+	github.com/go-playground/validator/v10 v10.5.0
 	github.com/go-zoo/bone v1.3.0
+	github.com/rosbit/reader-logger v0.0.0-20221216002729-c7c61d542c87
 	github.com/urfave/negroni v1.0.0
+	gopkg.in/yaml.v2 v2.2.8 //indirect
 )
